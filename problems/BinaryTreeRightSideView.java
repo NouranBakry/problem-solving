31d49c71a2ca7d0395cc80478ec39203c36bb207
NouranBakry <nouranaymanbakry@gmail.com>
2021-06-04 01:09:59 +0200

    bfs solution

--- problems/BinaryTreeRightSideView.java
+++ problems/BinaryTreeRightSideView.java
@@ -83,3 +83,45 @@ public class Solution {
         return result;
     }
 }
+/** submitted BFS*/ 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root == null){
+            return new ArrayList();
+        }
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        while(q.size()>0){
+            int size = q.size();
+            for(int i=0; i<size;i++){
+                TreeNode node = q.poll();
+                if(i == size-1){
+                    result.add(node.val);
+                }
+                if(node.left!=null){
+                    q.add(node.left);
+                }
+                if(node.right!=null){
+                    q.add(node.right);
+                }
+            }
+        }
+        return result;
+    }
+}
