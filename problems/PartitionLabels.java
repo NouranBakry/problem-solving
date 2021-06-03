601beb2cd618ccf7d0dead76f29edc1c633a9399
NouranBakry <nouranaymanbakry@gmail.com>
2021-06-04 00:21:36 +0200

    sol

--- problems/PartitionLabels.java
+++ problems/PartitionLabels.java
@@ -1,5 +1,20 @@
+// O(n), space O(26)-> O(1)
 class Solution {
-    public List<Integer> partitionLabels(String S) {
-
-    }
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> output = new ArrayList<>();
+        int[] map = new int[26];
+        for(int i=0; i<s.length();i++){
+            map[s.charAt(i)-'a'] = i;
+        }
+        int last = 0;
+        int start = 0;
+        for(int i=0; i< s.length();i++){
+            last = Math.max(last, map[s.charAt(i)-'a']);
+            if(i == last){
+                output.add(i - start+1);
+                start = i+1;
+            }
+        }
+        return output;
+    }    
 }
